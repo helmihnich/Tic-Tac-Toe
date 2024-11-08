8156197b6243bee4fadf27d7c27d1af2faa4018e
Helmi Hnich <hnich.helmi@gmail.com>
2024-11-09 00:38:38 +0100

    first commit

---
+++ src/app/board/board.component.ts
@@ -0,0 +1,31 @@
+import { Component,OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-board',
+  standalone: true,
+  imports: [],
+  templateUrl: './board.component.html',
+  styleUrl: './board.component.scss'
+})
+export class BoardComponent {
+  squares: any[] | undefined;
+  xIsNext: boolean | undefined;
+  winner: string | undefined;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.newGame();
+  }
+
+  newGame() {
+    this.squares = Array(9).fill(null);
+    this.winner = '';
+    this.xIsNext = true;
+  }
+
+  get player() {
+    return this.xIsNext ? 'X' : 'O';
+  }
+
+}
